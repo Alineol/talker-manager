00cc648ca2379d56fef8192ac7558ef8228f0cfc
Aline <allinne.oliveira.ol@gmail.com>
2022-05-16 18:53:19 -0300

    cria middleware para login

---
+++ middlewares/index.js
@@ -0,0 +1,22 @@
+function loginCheck(req, res, next) {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  const regex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+  const isValid = regex.test(email);
+  if (!isValid) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) { 
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  return next();
+}
+
+module.exports = {
+  loginCheck,
+};
