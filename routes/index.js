3fcb425c27adbff0dc3a4bbf674b7116e0fe46a9
Aline <allinne.oliveira.ol@gmail.com>
2022-05-16 17:43:28 -0300

    cria rota para talker/:id

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,15 @@ routes.get('/talker', async (req, res) => {
    return res.status(200).json(data);
 });
 
+routes.get('/talker/:id', async (req, res) => {
+  const data = await getTalker();
+  const { id } = req.params;
+  console.log(id);
+  const result = data.find((talker) => talker.id === Number(id));
+if (!result) {
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+}
+return res.status(200).json(result);
+});
+
 module.exports = routes;
