be1a1cd62792d5bde3ea868186750363b43b1327
Aline <allinne.oliveira.ol@gmail.com>
2022-05-17 16:32:19 -0300

    cria middlewares de validação req5

--- middlewares/index.js
+++ middlewares/index.js
@@ -19,7 +19,6 @@ function loginCheck(req, res, next) {
 
 function validateToken(req, res, next) {
   const { authorization } = req.headers;
-  console.log(authorization);
   if (!authorization) {
     return res.status(401).json({ message: 'Token não encontrado' });
   }
@@ -29,7 +28,65 @@ function validateToken(req, res, next) {
   return next();
 }
 
+ const validateName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name || name.length === 0) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age || age.length === 0) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const validateTalkDateRate = (req, res, next) => {
+  const { talk } = req.body;
+  const regex = /^([0-3][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+    const { watchedAt, rate } = talk;
+    const dateTest = regex.test(watchedAt);
+      if (!dateTest) {
+        return res.status(400).json({ message: 
+          'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+      } 
+      if (!Number.isInteger(rate) || rate < 1 || rate > 5) {
+        return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+      }
+      return next(); 
+    };
+
+  function validateTalk(req, res, next) {
+      const { talk } = req.body; 
+      if (!talk || !talk.rate || !talk.watchedAt) {
+        return res.status(400).json({ message: 
+          'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+      }
+      const { rate, watchedAt } = talk;
+      if (rate.length === 0) {
+        return res.status(400).json({ message: 
+          'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+      }
+      if (watchedAt.length === 0) {
+        return res.status(400).json({ message: 
+          'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+      }
+      return next();
+    }
 module.exports = {
   loginCheck,
   validateToken,
+  validateAge,
+  validateName,
+  validateTalkDateRate,
+  validateTalk,
 };
