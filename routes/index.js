c930881d804077c0f10b99bdaa8f5eb280d5c18d
Aline <allinne.oliveira.ol@gmail.com>
2022-05-17 19:17:45 -0300

    cria rota get/talker/search

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const { getTalker, generateToken, deleteTalker, addTalker, 
-  editTalker } = require('../helpers/index');
+  editTalker, findTalker } = require('../helpers/index');
 const { loginCheck, validateToken, validateAge, validateName, 
   validateTalk, validateTalkDateRate } = require('../middlewares/index');
 
 const routes = express.Router();
+
+routes.get('/talker/search', validateToken, async (req, res) => {
+  const { q } = req.query;
+  if (!q || q.length === 0) {
+    const talkers = await getTalker();
+    return res.status(200).json(talkers);
+  }
+  const find = await findTalker(q);
+  if (find === undefined) {
+    return res.status(200).json(['']);
+  }
+  res.status(200).json(find);
+});
 
 routes.get('/talker', async (req, res) => {
     const data = await getTalker();
@@ -14,6 +27,10 @@ routes.get('/talker', async (req, res) => {
    return res.status(200).json(data);
 });
 
+routes.post('/login', loginCheck, (req, res) => {
+  const token = generateToken(16);
+  return res.status(200).json({ token });
+});
 routes.get('/talker/:id', async (req, res) => {
   const data = await getTalker();
   const { id } = req.params;
@@ -22,11 +39,6 @@ if (!result) {
   return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 }
 return res.status(200).json(result);
-});
-
-routes.post('/login', loginCheck, (req, res) => {
-  const token = generateToken(16);
-  return res.status(200).json({ token });
 });
 
 routes.post('/talker', validateToken, validateAge, validateName, 
